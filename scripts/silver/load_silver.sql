d6fab4740f42d2a9c6e0f7db093d07c5d5a8c756
Fernando Filho <fernando.rsf23@gmail.com>
2025-07-26 19:09:02 +0100

    Updates silver.crm_sales_details date columns and refines ETL for data consistency
    - Changes data types for date columns in the silver schema to `DATE` for improved accuracy. Refines the ETL process in the load script to handle invalid dates, enforce data consistency across sales, quantity, and price, and ensure proper transformations from the bronze layer. Adds validations for unwanted spaces and key mismatches.
    
    - Enhances readability by documenting stored procedure usage and inserting structured comments. Improves reliability and correctness of data handling in the silver layer.

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -1,13 +1,20 @@
 /*
 ===============================================================================
-Load Script: Load Silver Layer (Bronze -> Silver)
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
 ===============================================================================
 Script Purpose:
-    This script performs the ETL (Extract, Transform, Load) process to 
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
     populate the 'silver' tables from the 'bronze' tables.
 	Actions Performed:
 		- Truncates Silver tables.
 		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC Silver.load_silver;
 ===============================================================================
 */
 
@@ -36,6 +43,7 @@ SELECT DISTINCT c.cst_gndr
 FROM bronze.crm_cust_info c 
 
 
+-- Populate the Table
 INSERT INTO silver.crm_cust_info (
 	cst_id,
 	cst_key,
@@ -83,7 +91,7 @@ HAVING unique_id > 1 OR c.prd_id IS NULL OR c.prd_id = 0;
 -- Check for Unwanted Spaces
 SELECT c.prd_nm 
 FROM bronze.crm_prd_info c 
-WHERE c.prd_nm != TRIM(c.prd_nm )
+WHERE c.prd_nm != TRIM(c.prd_nm)
 
 -- Check for NULLS or Negative Numbers
 SELECT c.prd_cost 
@@ -108,6 +116,7 @@ FROM bronze.crm_prd_info c
 WHERE c.prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509')
 
 
+-- Populate the Table
 INSERT INTO silver.crm_prd_info (
 	prd_id,
 	cat_key,
@@ -138,8 +147,115 @@ FROM bronze.crm_prd_info c
 -- WHERE SUBSTRING(c.prd_key, 7, LENGTH(c.prd_key)) IN (SELECT DISTINCT c.sls_prd_key FROM bronze.crm_sales_details c) -- check for the prd_key matching with bronze.crm_sales_details 
 
 
+/*
+===============================================================================
+CRM SALES DETAILS
+===============================================================================
+*/
+
+-- Check for Unwanted Spaces
+SELECT c.sls_ord_num 
+FROM bronze.crm_sales_details c 
+WHERE c.sls_ord_num != TRIM(c.sls_ord_num)
+
+-- Check for Matching Keys Between Tables
+SELECT c.sls_cust_id  
+FROM bronze.crm_sales_details c 
+WHERE c.sls_cust_id NOT IN (
+	SELECT cst_id FROM silver.crm_cust_info
+)
+
+-- Check for Invalid Dates
+SELECT
+	NULLIF(c.sls_order_dt, 0) sls_order_dt
+FROM bronze.crm_sales_details c
+WHERE c.sls_order_dt  <= 0 OR LENGTH(c.sls_order_dt) != 8
+
+-- Check for Invalid Date Orders
+SELECT 
+	*
+FROM bronze.crm_sales_details c 
+WHERE c.sls_order_dt  > c.sls_ship_dt OR c.sls_order_dt  > c.sls_due_dt
+
+-- Check Data Consistency: Between Sales, Quantity, and Price
+-- Sales = Quantity * Price -> Values must not be NULL, zero or negative
+-- Rules:
+-- 		1. If Sales are NEGATIVE, ZERO, or NULL, derive it from Quantity and Price
+-- 		2. If Price is ZERO or NULL, calculate it using Sales and Quantity
+-- 		3. If Prive is NEGATIVE, convert it to a positive value
+SELECT DISTINCT
+	c.sls_sales,
+	c.sls_quantity,
+	c.sls_price,
+	CASE
+    	WHEN (c.sls_sales IS NULL OR c.sls_sales <= 0)
+        	OR (
+            	c.sls_sales != c.sls_quantity * ABS(c.sls_price)
+            	AND c.sls_quantity > 0
+            	AND c.sls_price > 0
+         	)
+        		THEN c.sls_quantity * ABS(c.sls_price)
+    	ELSE c.sls_sales
+	END AS new_sls_sales,
+	CASE 
+        WHEN c.sls_price IS NULL OR c.sls_price = 0
+            THEN c.sls_sales / NULLIF(c.sls_quantity, 0)
+        WHEN c.sls_price < 0
+            THEN ABS(c.sls_price)
+        ELSE c.sls_price
+    END AS new_sls_price
+FROM bronze.crm_sales_details c 
+WHERE sls_sales != c.sls_quantity * c.sls_price 
+OR c.sls_sales <= 0 OR c.sls_quantity <= 0 OR c.sls_price <= 0
+OR c.sls_sales IS NULL OR c.sls_quantity IS NULL OR c.sls_price IS NULL
+ORDER BY c.sls_sales , c.sls_quantity , c.sls_price 
 
 
 
-
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+SELECT 
+	c.sls_ord_num,
+	c.sls_prd_key,
+	c.sls_cust_id,
+	CASE 
+        WHEN c.sls_order_dt = 0 OR LENGTH(c.sls_order_dt) != 8 THEN NULL
+        ELSE STR_TO_DATE(CAST(c.sls_order_dt AS CHAR), '%Y%m%d')
+    END sls_order_dt,
+    CASE 
+        WHEN c.sls_ship_dt = 0 OR LENGTH(c.sls_ship_dt) != 8 THEN NULL
+        ELSE STR_TO_DATE(CAST(c.sls_ship_dt AS CHAR), '%Y%m%d')
+    END sls_ship_dt,
+    CASE 
+        WHEN c.sls_due_dt = 0 OR LENGTH(c.sls_due_dt) != 8 THEN NULL
+        ELSE STR_TO_DATE(CAST(c.sls_due_dt AS CHAR), '%Y%m%d')
+    END sls_due_dt,
+	CASE
+    	WHEN (c.sls_sales IS NULL OR c.sls_sales <= 0)
+        	OR (
+            	c.sls_sales != c.sls_quantity * ABS(c.sls_price)
+            	AND c.sls_quantity > 0
+            	AND c.sls_price > 0
+         	)
+        		THEN c.sls_quantity * ABS(c.sls_price)
+    	ELSE c.sls_sales
+	END AS sls_sales,
+	c.sls_quantity,
+	CASE 
+        WHEN c.sls_price IS NULL OR c.sls_price = 0
+            THEN c.sls_sales / NULLIF(c.sls_quantity, 0)
+        WHEN c.sls_price < 0
+            THEN ABS(c.sls_price)
+        ELSE c.sls_price
+    END AS sls_price
+FROM bronze.crm_sales_details c 
 
